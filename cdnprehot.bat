22272dd60fc3efd1c3dd5adf52f72e80ae917ec5
shudingbo <shudingbo@qq.com>
2021-11-29 14:49:01 +0800

    简化批处理调用

--- cdnprehot.bat
+++ cdnprehot.bat
@@ -1,9 +1,10 @@
 @ECHO off
 echo %~1
 
-
+cd /d %~dp0
 node cdnprehot.js domain="https://*.com.cn/" path=%~1 mode=0 out=upa -v
 
 cd /D %~dp0
 node alicdn.js -f="C:\*\prehot-upa.txt" -i="accKeyId" -k="accKeySec" -n=10
+
 pause
